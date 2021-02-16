a98dfad125a02e8f59ca7a99135da614b96596a5
Aleksandr Pavlov <7911562+av-pavlov@users.noreply.github.com>
2021-02-16 09:28:19 +0900

    renames

---
+++ if-01.cpp
@@ -0,0 +1,19 @@
+/* 
+ 1. Напишите программу, которая вводит с клавиатуры одно целое число,
+    и выводит POL, если оно положительно, OTR — если отрицательно, и
+    NUL, если это нуль.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+
+	cin >> n;
+	if (n > 0) 
+		cout << "POL\n";
+	return  0;
+}
